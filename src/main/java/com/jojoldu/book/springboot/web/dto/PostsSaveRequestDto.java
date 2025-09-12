134be6744a72d554107b83dc04c6a863068d2642
Hoyeon Park <133315262+hoyeondev@users.noreply.github.com>
2025-09-12 17:32:02 +0900

    CURD 수정중

--- src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/jojoldu/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -5,17 +5,45 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+//@Getter
+//@NoArgsConstructor
+//public class PostsSaveRequestDto {
+//    private String title;
+//    private String content;
+//    private String author;
+//
+//    @Builder
+//    public PostsSaveRequestDto(String title, String content, String author) {
+//        this.title = title;
+//        this.content = content;
+//        this.author = author;
+//    }
+//
+//    public Posts toEntity() {
+//        return Posts.builder()
+//                .title(title)
+//                .content(content)
+//                .author(author)
+//                .build();
+//    }
+//
+//}
+
 @Getter
 @NoArgsConstructor
 public class PostsSaveRequestDto {
     private String title;
     private String content;
+    private String model;
+    private Boolean active;
     private String author;
 
     @Builder
-    public PostsSaveRequestDto(String title, String content, String author) {
+    public PostsSaveRequestDto(String title, String content, String model, Boolean active, String author) {
         this.title = title;
         this.content = content;
+        this.model = model;
+        this.active = active;
         this.author = author;
     }
 
@@ -23,8 +51,9 @@ public class PostsSaveRequestDto {
         return Posts.builder()
                 .title(title)
                 .content(content)
+                .model(model)
+                .active(active != null ? active : true)  // null인 경우 기본값 true
                 .author(author)
                 .build();
     }
-
 }
